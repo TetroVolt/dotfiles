b159ccb7337bc268c3739a58b42c363eb58cbf8a
TetroVolt <raymond@sutrisno.me>
2019-02-07 15:09:58 -0600

    updated dotfiles

--- packages.sh
+++ packages.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # wireless
-pacman -S networkmanager wpa_supplicant
+pacman -S networkmanager wpa_supplicant wireless_tools
 
 # intel stuff
 pacman -S intel-ucode mesa xf86-video-intel
@@ -9,17 +9,17 @@ pacman -S intel-ucode mesa xf86-video-intel
 # graphics
 pacman -S xorg xorg-init mesa
 
-# i3-gaps
+# i3
 pacman -S i3-gaps
 
 # dev tools
-pacman -S zsh termite vim git gcc python2 python3
+pacman -S zsh termite vim git gcc python2 python3 virtualenv
 
 # LaTeX
 pacman -S pandoc pdflatex biber
 
 # grub
 pacman -S grub
-#grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=grub
+grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=grub
 grub-mkconfig -o /boot/grub/grub.cfg
 
